8116f0c51395e37b9909671a851eb146ffcf5912
“MinAAib” <ab.mina.91@gmail.com>
2022-12-08 11:05:27 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * * main: print a string
+ *   *return: 0
+ */
+
+int main(void)
+{
+printf("%s", "with proper grammar, but theoutcome is a piece off art, \n");
+return (0);
+}
